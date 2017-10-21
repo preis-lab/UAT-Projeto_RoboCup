979345bbd8bc1efd9a3a874415892fca1e7470d1
Daniel Prates <danprates94@gmail.com>
2017-10-21 17:44:59 +0000

    correcao

--- SpaceCup/src/spacecup/Form/Main.java
+++ SpaceCup/src/spacecup/Form/Main.java
@@ -3,7 +3,7 @@ package spacecup.Form;
 public class Main extends javax.swing.JFrame {
 
     public static void main(String args[]) {
-        new FormLogin().setVisible(true);
-//        new MainForm().setVisible(true);
+//        new FormLogin().setVisible(true);
+        new MainForm().setVisible(true);
     }
 }
